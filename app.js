1ead3f166b9e34db470295241752809711702043
Zeeshan Choohan <99235041+zeeshan-choohan@users.noreply.github.com>
2023-04-06 20:20:08 +0500

    Add files via upload
    Sir I have some work so after few minutes I will update it init 2 questios are remain

---
+++ app.js
@@ -0,0 +1,106 @@
+'use strict'
+//   TASK 01
+
+// let num1 = prompt("Enter first number I will ask you which number is greater")
+// let num2 = prompt("Enter first number I will ask you which number is greater")
+// if(num1 > num2){
+//     console.log(num1+" is Greater")
+// }
+// else{
+//     console.log(num2+" is Greater")
+// }
+
+// TASK 02
+
+// let userInp = prompt("Enter any number I will ask you that its positive or Negative")
+// if(userInp > 0){
+//     console.log("Your given number is positive  + and its "+ userInp)
+// }
+// else if(userInp == 0){
+//     console.log("Your given number is positive  + and its "+ userInp)
+// }
+// else if(userInp < 0){
+//     console.log("Your given number is Negative - and its "+ userInp)
+// }
+
+
+//   TASK 03
+
+// let num1 = +prompt("Enter first number I ask you which is greater")
+// let num2 = +prompt("Enter second number I ask you which is greater")
+// let num3 = +prompt("Enter third number I ask you which is greater")
+// let num4 = +prompt("Enter fourth number I ask you which is greater")
+// let num5 = +prompt("Enter fifth number I ask you which is greater")
+
+// if(num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5){
+//  console.log("Number first "+num1+" is greater from second number "+num2+" and number three "+num3+" and number four "+num4+" and also num five "+num5)
+// }
+// else if(num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5){
+//     console.log("Number second "+num2+" is greater from first number "+num1+" and number three "+num3+" and number four "+num4+" and also num five "+num5)
+// }
+// else if(num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5){
+//     console.log("Number third "+num3+" is greater from first number "+num1+" and number two "+num2+" and number four "+num4+" and also num five "+num5)
+// }
+// else if(num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5){
+//     console.log("Number four "+num4+" is greater from first number "+num1+" and number two "+num2+" and number three "+num3+" and also num five "+num5)
+// }
+// else if(num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4){
+//     console.log("Number five "+num5+" is greater from first number "+num1+" and number two "+num2+" and number three "+num3+" and also num four "+num4)
+// }
+// else if(num1 === num2 ){
+//     console.log(num1+" first and second number"+num2+" are equal")
+// }
+// else if(num2 === num3 ){
+//     console.log(num2+" second and third number"+num3+" are equal")
+// }
+// else if(num3 === num4 ){
+//     console.log(num3+" third and fourth number"+num4+" are equal")
+// }
+// else if(num4 === num5 ){
+//     console.log(num4+" fourth and fifth number"+num5+" are equal")
+// }
+// else if(num1 === num2 && num1 ===  num3 && num1 === num4 && num1 === num5){
+//     console.log("All numbers all equal")
+// }
+// else{
+//     console.log("You type incorrect number")
+// }
+
+
+
+//   TASK 04
+
+// for(let i = 0; i <16 ; i++){
+//     if(i == 0){
+//         alert("Its Even number "+i)
+//     }
+//     else if(i%2){
+//         alert("number is even "+i)
+//     }
+//     else{
+//         alert("number is Odd "+i)
+//     }
+// }
+
+// TASK 05 
+
+// let userInp = +prompt("Enter your marks here!....")
+// if(userInp > 0 && userInp < 60 && userInp < 100){
+// console.table("<60 your Grade is F")
+// }
+// else if(userInp > 0 && userInp < 70 && userInp < 100){
+//     console.table("<70 your Grade is D")
+// }
+// else if(userInp > 0 && userInp < 80 && userInp < 100){
+//     console.table("<80 your Grade is C")
+// }
+// else if(userInp > 0 && userInp < 90 && userInp < 100){
+//     console.table("<90 your Grade is B")
+// }
+// else if(userInp > 0 && userInp < 100){
+//     console.table("<100 your Grade is A")
+// }
+
+
+
+
